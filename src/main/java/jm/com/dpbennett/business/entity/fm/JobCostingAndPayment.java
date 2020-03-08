e3b4067988d348fe633d325fa9dec3b09d275ea5
dbennett <dbennett@DESKTOP50.BOS.local>
2020-03-08 09:29:40 -0500

    General updates.

--- src/main/java/jm/com/dpbennett/business/entity/fm/JobCostingAndPayment.java
+++ src/main/java/jm/com/dpbennett/business/entity/fm/JobCostingAndPayment.java
@@ -858,6 +858,38 @@ public class JobCostingAndPayment implements Serializable, BusinessEntity {
 
         return null;
     }
+    
+     public static JobCostingAndPayment findActiveJobCostingAndPaymentByDepartmentAndName(
+            EntityManager em,
+            String departmentName,
+            String jobCostingAndPaymentName) {
+
+        try {
+            String newJobCostingAndPaymentName = jobCostingAndPaymentName.replaceAll("'", "''");
+            String newDepartmentName = departmentName.replaceAll("'", "''");
+
+            List<Job> jobs
+                    = em.createQuery("SELECT job FROM Job job"
+                            + " JOIN job.jobCostingAndPayment jobCostingAndPayment"
+                            + " JOIN job.department department"
+                            + " JOIN job.subContractedDepartment subContractedDepartment"
+                            + " WHERE UPPER(jobCostingAndPayment.name) = '"
+                            + newJobCostingAndPaymentName.toUpperCase().trim() + "'"
+                            + " AND ( UPPER(department.name) = '" + newDepartmentName.toUpperCase() + "'"
+                            + " OR UPPER(subContractedDepartment.name) = '" + newDepartmentName.toUpperCase() + "'"
+                            + " )"
+                            + " AND (jobCostingAndPayment.active = 1 OR jobCostingAndPayment.active IS NULL)"
+                            + " ORDER BY jobCostingAndPayment.name", Job.class).getResultList();
+            if (!jobs.isEmpty()) {
+                return jobs.get(0).getJobCostingAndPayment();
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+
+        return null;
+    }
 
     public static JobCostingAndPayment findJobCostingAndPaymentById(EntityManager em, Long Id) {
         return em.find(JobCostingAndPayment.class, Id);
