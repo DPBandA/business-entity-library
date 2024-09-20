b45698ad79329521dd07a3f467151564518d76b9
Desmond Bennett <Desmond Bennett@DESKTOP-H30NB01>
2024-09-20 18:02:53 -0500

    General updates.

---
+++ src/main/java/jm/com/dpbennett/business/entity/dm/Issue.java
@@ -0,0 +1,62 @@
+/*
+Business Entity Library (BEL) - A foundational library for JSF web applications 
+Copyright (C) 2024  D P Bennett & Associates Limited
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Email: info@dpbennett.com.jm
+ */
+package jm.com.dpbennett.business.entity.dm;
+
+/**
+ *
+ * @author Desmond Bennett
+ */
+public class Issue {
+
+    private String title;
+    private String type;
+    private String body;
+
+    public Issue() {
+        title = "";
+        type = "";
+        body = "";
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+    
+}
