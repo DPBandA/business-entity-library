8c6517f0196471283ab4f51a5ababccbe6f2db36
desbenn <desbenn@adassa>
2026-09-20 13:08:18 -0500

    General updates.

---
+++ src/test/java/jm/com/dpbennett/business/entity/test/GenericsTest.java
@@ -0,0 +1,41 @@
+/*
+Business Entity Library (BEL) 
+Copyright (C) 2026  D P Bennett & Associates Limited
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Email: info@dpbennett.com.jm
+ */
+package jm.com.dpbennett.business.entity.test;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import jm.com.dpbennett.business.entity.cm.Client;
+import org.junit.Test;
+
+/**
+ *
+ * @author Desmond Bennett
+ */
+public class GenericsTest {
+
+    @Test
+    public void testEntity() {
+        
+        
+
+    }
+}
